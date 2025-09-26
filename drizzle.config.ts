7634925a5be1caafa964b6b27236ec39f0259f62
tsunakit99 <tainosuke0926@gmail.com>
2025-09-27 03:01:34 +0900

    fix: schema.tsのインポートパス修正

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle/migrations",
-  schema: "./database/schema.ts",
+  schema: "./lib/database/schema.ts",
   dialect: "sqlite",
   driver: "d1-http",
   dbCredentials: {
